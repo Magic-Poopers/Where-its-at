c9c3bc9007fe12d8efe4088a64a4316ffa4fad6a
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2023-04-11 16:40:16 +0200

    routes

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Header from "../components/Header";
+import EventInfo from "../components/EventInfo";
+import styles from "./Event.module.css";
+import PrimaryButton from "../components/PrimaryButton";
 
 export const Event = () => {
-  return <div>Event</div>;
-};
+  const event = {
+    name: "Lasse-Stefanz",
+    price: 400,
+    where: "Avicii Arena",
+    when: {
+      date: "21 Mar",
+      from: "20:00",
+      to: "22:00"
+    }
+  };
 
-export default Event;
+  function handleClick() {
+    // Lägg till i orderItems
+    // Navigera till varukorg
+    console.log("Clicked!");
+  }
+
+  return (
+    <main className={styles.eventPage}>
+      <section>
+        <Header title={"Event"} />
+        <p className={styles.paragraph}>
+          You are about to score some tickets to
+        </p>
+      </section>
+      <EventInfo event={event} />
+      <PrimaryButton children={"Lägg i varukorg"} onClick={handleClick} />
+    </main>
+  );
+};
