d1e492af8e19560ed76acce982559407e1be3fd8
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2023-04-11 11:09:18 +0200

    first commit

---
+++ src/pages/AllEvents.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AllEvents = () => {
+  return <div>AllEvents</div>;
+};
---
+++ src/pages/AllTickets.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AllTickets = () => {
+  return <div>AllTickets</div>;
+};
