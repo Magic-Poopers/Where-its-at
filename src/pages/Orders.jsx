53366fea9ff214b930d8a5931decb9770124ae60
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-04-17 13:40:58 +0200

    Disabled Send Order btn if cart is empty

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -9,6 +9,7 @@ import { useNavigate } from "react-router-dom";
 const Orders = () => {
   const { cart, copyTickets } = useCart();
   const navigate = useNavigate();
+  let isDisabled = false;
 
   const calculateTotal = () => {
     return cart
@@ -23,13 +24,18 @@ const Orders = () => {
   const items = cart.map((item, index) => {
     return <OrderItem event={item} key={index} />;
   });
+
+  if (cart.length < 1) {
+    isDisabled = true;
+  }
+
   return (
     <div className={styles.container}>
       <Header title={"Order"} />
       {items}
       <p className={styles.total}>Totalt värde på order</p>
       <p className={styles.price}>{calculateTotal()} sek</p>
-      <PrimaryButton onClick={handleClick}>Skicka order</PrimaryButton>
+      <PrimaryButton onClick={handleClick} isDisabled={isDisabled}>Skicka order</PrimaryButton>
     </div>
   );
 };
